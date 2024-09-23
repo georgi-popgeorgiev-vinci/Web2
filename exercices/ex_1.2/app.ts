1d58eb47670723100d56473bc3b8f30c599e4f64
Popgeorgiev <georgi.popgeorgiev@student.vinci.be>
2024-09-23 22:22:48 +0200

    new ex1.2 middlewares

--- exercices/ex_1.2/app.ts
+++ exercices/ex_1.2/app.ts
@@ -1,25 +1,17 @@
 import express from "express";
+import requestCount from "./middlewares/requestCount";
 
 
 import filmsRouter from "./routes/films";
 
 const app = express();
-let count = 0;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-
-app.use((req, _res, next) => {
-    if(req.method === "GET") {
-        count++;
-        console.log(`Number of GET requests: ${count}`);
-    }
-    next();
-});
-
+app.use(requestCount);
 app.use("/films", filmsRouter);
+
 
 
 
---
+++ exercices/ex_1.2/middlewares/requestCount.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+
+let count = 0;
+
+const requestCount = (req : Request, _res : Response, next : NextFunction) => {
+    if(req.method === "GET") {
+        count++;
+        console.log(`Number of GET requests: ${count}`);
+    }
+    next();
+};
+
+export default requestCount;
